811157ed4b8eae3f9238e3d1337f55365bd47fcd
Esau Acuña <ldranzer4@gmail.com>
2021-05-25 21:04:16 -0500

    ProyectoBaseUP

---
+++ src/javaevfinal/Cita.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaevfinal;
+
+/**
+ *
+ * @author a
+ */
+public class Cita {
+    int id;
+    String day;
+
+    public Cita(int id, String day) {
+        this.id = id;
+        this.day = day;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+    
+    
+}
---
+++ src/javaevfinal/JAVAEVFINAL.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaevfinal;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author a
+ */
+public class JAVAEVFINAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    Scanner sc = new Scanner(System.in);
+    
+     public void menu() throws FileNotFoundException{
+        System.out.println("Ingrese la operación a realizar");
+        System.out.println("1. Dar de alta doctores");
+        System.out.println("2. Dar de alta pacientes");
+        System.out.println("3. Crear citas");
+        System.out.println("4. Relacionar citas");
+        
+        int opcion;
+        opcion = sc.nextInt();
+        switch(opcion){
+            case 1:
+                darAltaDoctores();
+                break;
+            case 2:
+                darAltaPacientes();
+                break;
+            case 3:
+                crearCitas();
+                break;
+            case 4:
+                relacionarCitas();
+                break;
+            default:
+                System.out.println("Error");
+        }
+        
+    }
+    public static void main(String[] args) throws FileNotFoundException{
+        // TODO code application logic here
+        
+        JAVAEVFINAL menu = new JAVAEVFINAL();
+        menu.mainmenu();
+        
+    }
+    
+    public void mainmenu() throws FileNotFoundException{
+        System.out.println("Ingrese la operación a realizar");
+        System.out.println("1. Dar de alta doctores");
+        System.out.println("2. Dar de alta pacientes");
+        System.out.println("3. Crear citas");
+        System.out.println("4. Relacionar citas");
+        
+        int opcion;
+        opcion = sc.nextInt();
+        switch(opcion){
+            case 1:
+                darAltaDoctores();
+                break;
+            case 2:
+                darAltaPacientes();
+                break;
+            case 3:
+                crearCitas();
+                break;
+            case 4:
+                relacionarCitas();
+                break;
+            default:
+                System.out.println("Error");
+        }
+        
+    }
+    
+    public void darAltaDoctores() throws FileNotFoundException{
+        int id;
+        String name;
+        System.out.println("Ingrese Id del Doctor");
+        id=sc.nextInt();
+        System.out.println("Ingrese nombre del Doctor");
+        name=sc.next();
+        Doctor doctor = new Doctor(id, name);
+        
+        try (PrintWriter out = new PrintWriter("filenameDoctors.txt")) {
+        out.println(id);
+        out.println(name);
+}
+    }
+    
+    public void darAltaPacientes() throws FileNotFoundException{
+        int id;
+        String name;
+        System.out.println("Ingrese Id del Paciente");
+        id=sc.nextInt();
+        System.out.println("Ingrese nombre del Paciente");
+        name=sc.next();
+        Paciente paciente = new Paciente(id, name);
+        try (PrintWriter out = new PrintWriter("filenamePacientes.txt")) {
+        out.println(id);
+        out.println(name);
+    }}
+    
+    public void crearCitas() throws FileNotFoundException{
+        int id;
+        String dates;
+        System.out.println("Ingrese Id de cita");
+        id=sc.nextInt();
+        System.out.println("Ingrese fecha de cita");
+        dates=sc.next();
+        Cita cita = new Cita(id, dates);
+               try (PrintWriter out = new PrintWriter("filenameCitas.txt")) {
+        out.println(id);
+        out.println(dates);
+       
+    }}
+    public void relacionarCitas() throws FileNotFoundException{
+        int idDoc, idPac, idCita;
+        System.out.println("Ingrese ID de Doctor");
+        idDoc = sc.nextInt();
+        System.out.println("Ingrese ID de Paciente");
+        idPac = sc.nextInt();
+        System.out.println("Ingrese ID de Cita");
+        idCita = sc.nextInt();
+        System.out.println("Datos guardados");
+        try (PrintWriter out = new PrintWriter("filenameRelacionados.txt")) {
+        out.println(idDoc);
+        out.println(idPac);
+        out.println(idCita);
+        
+        
+    }}
+}
